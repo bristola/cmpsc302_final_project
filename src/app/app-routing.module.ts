20ee16b61a8c5c20315348c06f228a713b304526
bristola <bristola@allegheny.edu>
2019-04-04 22:11:02 -0400

    Fully working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AboutComponent } from "./about/about.component";
 
 
 const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: "full"},
     {path: 'home', component: HomeComponent},
     {path: 'features', component: FeaturesComponent},
     {path: 'about', component: AboutComponent}
