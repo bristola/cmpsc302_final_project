9320aceedc6ec74179704f95b4cd4a89b81440a0
bristola <bristola@allegheny.edu>
2019-04-05 13:12:45 -0400

    Animations are smooth now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,62 +4,82 @@ import { RouterOutlet } from '@angular/router';
 import { trigger, transition, animate, style, query, animateChild, group } from '@angular/animations'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-      trigger('routeAnimations', [
-    transition('AboutPage <=> HomePage', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
-      query(':enter', [
-        style({ left: '-100%'})
-      ]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [
-          animate('500ms ease-out', style({ left: '100%'}))
-        ]),
-        query(':enter', [
-          animate('500ms ease-out', style({ left: '0%'}))
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    animations: [
+        trigger('routeAnimations', [
+            // Home page to home page
+            transition('FeaturesPage <=> AboutPage', [
+                style({ position: 'relative' }),
+                query(':enter, :leave', [
+                    style({
+                        position: 'absolute',
+                        opacity: 0.0
+                    })
+                ]),
+                query(':enter', [
+                    style({ opacity: 0.0})
+                ]),
+                query(':leave', animateChild()),
+                group([
+                    query(':leave', [
+                        animate('1000ms ease-out', style({ opacity: 0.0 }))
+                    ]),
+                    query(':enter', [
+                        animate('1000ms ease-out', style({ opacity: 1.0 }))
+                    ])
+                ]),
+                query(':enter', animateChild()),
+            ]),
+            // About page to about page
+            transition('About <=> HomePage', [
+                style({ position: 'relative' }),
+                query(':enter, :leave', [
+                    style({
+                        position: 'absolute',
+                        opacity: 0.0
+                    })
+                ]),
+                query(':enter', [
+                    style({ opacity: 0.0})
+                ]),
+                query(':leave', animateChild()),
+                group([
+                    query(':leave', [
+                        animate('1000ms ease-out', style({ opacity: 0.0 }))
+                    ]),
+                    query(':enter', [
+                        animate('1000ms ease-out', style({ opacity: 1.0 }))
+                    ])
+                ]),
+                query(':enter', animateChild()),
+            ]),
+            // Features page to about page
+            transition('FeaturesPage <=> HomePage', [
+                style({ position: 'relative' }),
+                query(':enter, :leave', [
+                    style({
+                        position: 'absolute',
+                        opacity: 0.0
+                    })
+                ]),
+                query(':enter', [
+                    style({ opacity: 0.0})
+                ]),
+                query(':leave', animateChild()),
+                group([
+                    query(':leave', [
+                        animate('1000ms ease-out', style({ opacity: 0.0 }))
+                    ]),
+                    query(':enter', [
+                        animate('1000ms ease-out', style({ opacity: 1.0 }))
+                    ])
+                ]),
+                query(':enter', animateChild()),
+            ]),
         ])
-      ]),
-      query(':enter', animateChild()),
-    ]),
-    transition('HomePage <=> About', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          color: 'red'
-        })
-      ]),
-      query(':enter', [
-        style({ left: '-100%'})
-      ]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [
-          animate('500ms ease-out', style({ left: '100%'}))
-        ]),
-        query(':enter', [
-          animate('500ms ease-out', style({ left: '0%'}))
-        ])
-      ]),
-      query(':enter', animateChild()),
-    ])
-  ])
-  ]
+    ]
 })
 export class AppComponent {
   title = 'Azariel Technologies';
